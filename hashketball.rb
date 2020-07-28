048fee888109414e780441ae65a6f44c6bfc122f
Lida Attenborough <attenboroughl@protonmail.com>
2020-07-28 21:30:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,133 @@
-# Write your code here!
+#def game_hash
+     {
+         home: {
+             team_name: "Brooklyn Nets",
+             colors: [Black, White],
+             players: [
+                 {
+                    Player Name: "Alan Anderson",
+                    Number: 0,
+                    Shoe: 16,
+                    Points: 22,
+                    Rebounds: 12,
+                    Assists: 12,
+                    Steals: 3,
+                    Blocks: 1,
+                    Slam Dunks: 1
+                 },
+                 {
+                    Player Name: "Reggie Evans",
+                    Number: 30,
+                    Shoe: 14,
+                    Points: 12,
+                    Rebounds: 12,
+                    Assists: 12,
+                    Steals: 12,
+                    Blocks: 12,
+                    Slam Dunks: 7
+                 },
+                 {
+                    Player Name: "Brook Lopez",
+                    Number: 11,
+                    Shoe: 17,
+                    Points: 17,
+                    Rebounds: 19,
+                    Assists: 10,
+                    Steals: 3,
+                    Blocks: 1,
+                    Slam Dunks: 15
+                 },
+                 {
+                    Player Name: "Mason Plumlee",
+                    Number: 1,
+                    Shoe: 19,
+                    Points: 26,
+                    Rebounds: 11,
+                    Assists: 6,
+                    Steals: 3,
+                    Blocks: 8,
+                    Slam Dunks: 5
+                 },
+                 {
+                    Player Name: "Jason Terry",
+                    Number: 31,
+                    Shoe: 15,
+                    Points: 19,
+                    Rebounds: 2,
+                    Assists: 2,
+                    Steals: 4,
+                    Blocks: 11,
+                    Slam Dunks: 1
+                 }
+                ],
+             },
+
+
+
+        away: {
+            team_name: "Charlotte Hornets",
+            colors: [Turquoise, Purple],
+            players: [
+                {
+                    Player Name: "Jeff Adrien",
+                    Number: 4,
+                    Shoe: 18,
+                    Points: 10,
+                    Rebounds: 1,
+                    Assists: 1,
+                    Steals: 2,
+                    Blocks: 7,
+                    Slam Dunks: 2
+                  },
+                  {
+                    Player Name: "Bismack Biyombo",
+                    Number: 0,
+                    Shoe: 16,
+                    Points: 12,
+                    Rebounds: 4,
+                    Assists: 7,
+                    Steals: 22,
+                    Blocks: 15,
+                    Slam Dunks: 10
+                  },
+                  {
+                    Player Name: "DeSagna Diop",
+                    Number: 2,
+                    Shoe: 14,
+                    Points: 24,
+                    Rebounds: 12,
+                    Assists: 12,
+                    Steals: 4,
+                    Blocks: 5,
+                    Slam Dunks: 5
+                  },
+                  {
+                    Player Name: "Ben Gordon",
+                    Number: 8,
+                    Shoe: 15,
+                    Points: 33,
+                    Rebounds: 3,
+                    Assists: 2,
+                    Steals: 1,
+                    Blocks: 1,
+                    Slam Dunks: 0
+                  },
+                  {
+                    Player Name: "Kemba Walker",
+                    Number: 33,
+                    Shoe: 15,
+                    Points: 6,
+                    Rebounds: 12,
+                    Assists: 12,
+                    Steals: 7,
+                    Blocks: 5,
+                    Slam Dunks: 12
+                  }
+
+                ]
+
+         }
+
+}
+end
+ Write your code here!
